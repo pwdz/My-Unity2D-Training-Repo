9ff32f8816e6acfe61ad3af3f56e0f63b3963563
Ebi <9731002@ceit.local>
2019-11-14 17:57:28 +0330

    script added

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField]Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "adibibibibibibibibibibib";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
