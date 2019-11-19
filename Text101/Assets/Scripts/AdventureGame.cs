779c5db752560d525c5ff9009716266a6ed98561
Ebi <9731002@ceit.local>
2019-11-19 23:32:55 +0330

    one font added and Adventure.cs modified

--- Text101/Assets/Scripts/AdventureGame.cs
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -23,10 +23,10 @@ public class AdventureGame : MonoBehaviour
     private void StateManager()
     {
         var nextStates = state.GetNextStates();
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-            state = nextStates[0];
-        else if(Input.GetKeyDown(KeyCode.Alpha2))
-            state = nextStates[1];
+        for(int i=0;i<nextStates.Length;i++)
+            if(Input.GetKeyDown(KeyCode.Alpha1+i))
+                state = nextStates[i];
+        
         textComponent.text = state.GetStateStory();    
     }
     
